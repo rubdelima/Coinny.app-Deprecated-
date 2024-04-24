f379050f6959d47d37cd09b0a755d9366297fec0
rubdelima <rubensndelima@hotmail.com>
2024-04-23 21:06:12 -0300

    refact: add components colors

---
+++ lib/utils/coinny_colors.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+
+class ComponentsColors {
+  LinearGradient lockCard = const LinearGradient(
+      colors: [Color(0xFF878787),Color(0xFF313131) ], begin: Alignment.topLeft, end: Alignment.bottomRight);
+
+  LinearGradient teste = const LinearGradient(
+      colors: [Color(0xFF1C296B),Color(0xFF59E7D6) ], begin: Alignment.topLeft, end: Alignment.bottomRight);
+
+  LinearGradient blueCard = const LinearGradient(
+      colors: [Color(0xFF7A7FFF),Color(0xFF373CA7), Color(0xFF040862) ], begin: Alignment.topLeft, end: Alignment.bottomRight);
+
+  LinearGradient greenCard = const LinearGradient(
+      colors: [Color(0xFF1290A2),Color(0xFF82DA59) ], begin: Alignment.topLeft, end: Alignment.bottomRight);
+
+  LinearGradient activities = const LinearGradient(
+      colors: [Color(0xFF7A7FFF),Color(0xFF040862) ], begin: Alignment.topCenter, end: Alignment.bottomCenter);
+
+  LinearGradient conquist = const LinearGradient(
+      colors: [Color(0xFF2F12A2),Color(0xFFDA59AE) ], begin: Alignment.topLeft, end: Alignment.bottomRight);
+
+  LinearGradient button = const LinearGradient(
+      colors: [Color(0xFF646AE3),Color(0xFF262B91) ], begin: Alignment.topLeft, end: Alignment.bottomRight);
+
+  LinearGradient card = const LinearGradient(
+      colors: [Color(0xFF1E12A2),Color(0xFF59CBDA) ], begin: Alignment.topLeft, end: Alignment.bottomRight);
+
+  LinearGradient yellowCard = const LinearGradient(
+      colors: [Color(0xFFA24E12),Color(0xFFF9DA6F) ], begin: Alignment.topLeft, end: Alignment.bottomRight);
+
+  LinearGradient ligthBlue = const LinearGradient(
+      colors: [Color(0xFF1277A2),Color(0xFF59DAA4) ], begin: Alignment.topLeft, end: Alignment.bottomRight);
+  
+  LinearGradient red = const LinearGradient(
+      colors: [Color(0xFF910B0B),Color(0xFFDA5959) ], begin: Alignment.topLeft, end: Alignment.bottomRight);
+
+  LinearGradient ligthCyan = const LinearGradient(
+      colors: [Color(0xFF59D2DA),Color(0xFF1232A2) ], begin: Alignment.topLeft, end: Alignment.bottomRight);
+}
